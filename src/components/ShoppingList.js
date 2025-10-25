d995773aed50387eab5149ce6025574aac74cb64
Rodney Swaji <rodney.swaji@student.moringaschool.com>
2025-10-26 01:31:30 +0300

    fixed an error

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,20 +1,25 @@
 import React, { useEffect, useState } from "react";
 import Item from "./Item";
 import ItemForm from "./ItemForm";
+import Filter from "./Filter";
 
 function ShoppingList() {
   const [items, setItems] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("All");
 
+  // 🟢 READ - fetch items
   useEffect(() => {
-    fetch("/items")
+    fetch("http://localhost:4000/items")
       .then((r) => r.json())
       .then((data) => setItems(data));
   }, []);
 
+  // 🟢 CREATE
   function handleAddItem(newItem) {
     setItems([...items, newItem]);
   }
 
+  // 🟢 UPDATE
   function handleUpdateItem(updatedItem) {
     const updatedItems = items.map((item) =>
       item.id === updatedItem.id ? updatedItem : item
@@ -22,16 +27,27 @@ function ShoppingList() {
     setItems(updatedItems);
   }
 
+  // 🟢 DELETE
   function handleDeleteItem(deletedItem) {
     const updatedItems = items.filter((item) => item.id !== deletedItem.id);
     setItems(updatedItems);
   }
 
+  function handleCategoryChange(category) {
+    setSelectedCategory(category);
+  }
+
+  const itemsToDisplay =
+    selectedCategory === "All"
+      ? items
+      : items.filter((item) => item.category === selectedCategory);
+
   return (
     <div className="ShoppingList">
       <ItemForm onAddItem={handleAddItem} />
-      <ul>
-        {items.map((item) => (
+      <Filter category={selectedCategory} onCategoryChange={handleCategoryChange} />
+      <ul className="Items">
+        {itemsToDisplay.map((item) => (
           <Item
             key={item.id}
             item={item}
@@ -45,4 +61,3 @@ function ShoppingList() {
 }
 
 export default ShoppingList;
-
